3244732d635ce734a7a6bc5eb717c90d352a4767
20Mugdha <144349731+20Mugdha@users.noreply.github.com>
2024-09-27 21:11:23 +0530

    Create TipGenerator.py

---
+++ TipGenerator.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+bill_with_tip= tip/100*bill+bill
+bill_per_person= bill_with_tip/people
+total= round(bill_per_person, 2)
+print(f"The total amount that a person is supposed to pay is: ${total}")
+
+
